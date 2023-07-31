1b7eb34f68e994ed5c33dd81daa094938c18565d
mariya <cawaforest@gmail.com>
2023-07-31 20:58:50 +0300

    Добавили задачи для второй домашней работы

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -61,3 +61,91 @@ while (count <= num)
     count++;
 }
 */
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+/*
+int InputNum(string ourNum)
+{
+    Console.Write(ourNum);
+    int InNum = Convert.ToInt32(Console.ReadLine());
+    return InNum;
+}
+Repeat:
+int number = InputNum("Введите трехзначное число: ");
+if (number < 100 || number >= 1000)
+{
+    Console.WriteLine("Это не трехзначное число, повторите ввод.");
+    goto Repeat;
+}
+
+Console.WriteLine($"Введенное число {number}, вторая цифра числа {number / 10 % 10}.");
+*/
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа ( или -1, если третьей цифры нет).
+/*
+int InputNum(string ourNum)
+{
+    Console.Write(ourNum);
+    int InNum = Convert.ToInt32(Console.ReadLine());
+    return InNum;
+}
+
+bool CheckNum(int InNum)
+{
+    if (InNum < 100)
+    {
+        Console.WriteLine("Третьей цифры нет!");
+        return false;
+    }
+    return true;
+}
+
+int ThirdDig(int inNum)
+{
+   while (inNum >= 1000)
+   {
+    inNum /= 10;
+   }
+   return inNum % 10;
+}
+
+int number = InputNum("Введите число: ");
+if (CheckNum(number))
+{
+    Console.WriteLine($"В числе {number} третья цифра {ThirdDig(number)}");
+}
+*/
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+/*
+int InputNum(string ourNum)
+{
+    Console.Write(ourNum);
+    int InNum = Convert.ToInt32(Console.ReadLine());
+    return InNum;
+}
+
+bool WeekEnd(int numDay)
+{
+    if (numDay > 5)
+    {
+        return true;
+    }
+    return false;
+}
+
+Repeat:
+int day = InputNum("Введите день недели: ");
+
+if (day < 1 || day > 7)
+    {
+        Console.WriteLine("Это не день недели.");
+        goto Repeat;
+    }
+
+if (WeekEnd(day))
+{
+    Console.WriteLine("Сегодня выходной!");
+}
+else
+{
+    Console.WriteLine("Сегодня рабочий день.");
+}
+*/
