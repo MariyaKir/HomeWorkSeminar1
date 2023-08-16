74a05c0d46afec930626901ab739c28edbaf22f5
mariya <cawaforest@gmail.com>
2023-08-16 18:46:08 +0300

    Добавили 5 домашнюю работу

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -295,3 +295,113 @@ Console.WriteLine("Массив, введенный пользователем: 
 OutArray(numElem, inArray);
 Console.WriteLine();
 */
+
+// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+/*
+int[] CreateRandomArray(int size)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(100, 1000);
+
+    return array;
+}
+void WriteArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+int QuantityOfEvenNum(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] % 2 == 0) count++;
+    return count;
+}
+Console.Write("Input a length of array: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] myArray = CreateRandomArray(m);
+WriteArray(myArray);
+Console.WriteLine(QuantityOfEvenNum(myArray));
+*/
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+/*
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for(int i = 0; i < size; i++)
+        array[i] = new Random().Next(minValue, maxValue + 1);
+
+    return array;
+}
+void WriteArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+int SumOfNegativ(int[] array)
+{
+    int sum = 0;
+    for(int i = 1; i < array.Length; i += 2)
+        sum += array[i];
+    return sum;
+}
+
+Console.Write("input a length of array: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateRandomArray(m, a, b);
+WriteArray(myArray);
+Console.WriteLine(SumOfNegativ(myArray));
+*/
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+/*
+double[] CreateRandomArray(int size)
+{
+    double[] array = new double[size];
+    for (int i = 0; i < size; i++)
+        {
+            Console.Write($"Input number in array with index {i}: ");
+            array[i] = Convert.ToDouble(Console.ReadLine());
+        }
+    return array;
+}
+void WriteArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+double DifferenceMaxMin(double[] array)
+{
+    double result = 0;
+    double min = array[0];
+    double max = array[0];
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > max)
+            max = array[i];
+        if(array[i] < min)
+            min = array[i];
+        result = max - min;
+    }
+    return result;
+}
+Console.Write("Input a length of array: ");
+int m = Convert.ToInt32(Console.ReadLine());
+double[] myArray = CreateRandomArray(m);
+WriteArray(myArray);
+Console.WriteLine(DifferenceMaxMin(myArray));
+*/
